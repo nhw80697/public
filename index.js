f345c71489019d6ddf2be144d3c5fe199b2db20c
naf <nhw80697@gmail.com>
2020-08-03 16:22:37 +0300

    קבצים ראשונים

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const PORT = process.env.PORT || 5000
+
+
+
+var con = mysql.createConnection({
+  host: "sql7.freemysqlhosting.net",
+  user: "sql7357169",
+  password: "KlA2Wh7suy",
+  database: "sql7357169"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.all('/', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS'),
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+    con.query("SELECT * FROM tt_table", function (err, result, fields) {
+      // if (err) throw err;
+      res.send(result);
+    });
+});
