38a27b0c9e0909541e4fd9bd93862fc24fdcc6e6
naf <nhw80697@gmail.com>
2020-08-03 17:12:41 +0300

    תיקון

--- index.js
+++ index.js
@@ -5,24 +5,25 @@ const PORT = process.env.PORT || 5000
 
 
 
-var con = mysql.createConnection({
-  host: "sql7.freemysqlhosting.net",
-  user: "sql7357169",
-  password: "203564778",
-  database: "sql7357169"
-});
+// var con = mysql.createConnection({
+//   host: "sql7.freemysqlhosting.net",
+//   user: "sql7357169",
+//   password: "203564778",
+//   database: "sql7357169"
+// });
 
-con.connect(function(err) {
-  if (err) throw err;
-});
+// con.connect(function(err) {
+//   if (err) throw err;
+// });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 app.all('/', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS'),
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
-    con.query("SELECT * FROM tt_table", function (err, result, fields) {
-      // if (err) throw err;
-      res.send(result);
-    });
+    // con.query("SELECT * FROM tt_table", function (err, result, fields) {
+    //   // if (err) throw err;
+    //   res.send(result);
+    // });
+    res.send("hello")
 });
